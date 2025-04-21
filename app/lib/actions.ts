7c40e37aaa05c71fbe51729a45b80e56cd948101
Domiciano <guilhermedomicianosilva@gmail.com>
2025-04-21 01:43:25 -0300

    delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -105,9 +105,9 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  //throw new Error('Failed to Delete Invoice');
  
-  // Unreachable code block
+  // Unreachable code block\
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
